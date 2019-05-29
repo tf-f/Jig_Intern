e10a86b41b7e54d4045388594d1006e5a9f61f97
Tomomichi Fuse <ec28115i@st.nagaoka-ct.ac.jp>
2019-05-30 02:18:56 +0900

    もっと見るの実装
    selectバグってる...

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //import Vue from 'vue';
 //import axios from 'axios';
 //require('es6-promise').polyfill();
-/* Json Reader ??????? link
+/* Json Reader 参考サイトなど link
 https://jp.vuejs.org/v2/cookbook/using-axios-to-consume-apis.html
 https://qiita.com/knaito-531/items/50935571e908a6425514
 https://ginpen.com/2016/12/11/how-to-get-values-from-form-with-vue/
@@ -13,17 +13,28 @@ const URL_ = ('json/test.json');
 var JsonReader = new Vue({
   el: '#app',
   data:{
-    datalists: [], //json ????
-    selected_size:1000,
+    datalists: [], //json 読み込み
+    select_value:[
+      {text: '全て表示' ,value: 1000},
+      {text: '10件表示', value: 10},
+      {text: '20件表示', value: 20},
+      {text: '30件表示', value: 30},
+      {text: '40件表示', value: 40}
+    ],
     selected_genre: 'All',    
-    // more button ???????
-    first :1000, // ????
-    count: 5, //more??????????????
-    step:0 // more ???????
+    // more button の動作について
+    // 初期行数
+    selected_size:10,
+    count: 5, //moreが押されたとき追加される行数
+    step:0 // more が押された回数
   },
   computed: {
+
+    ee:function(){
+      return this.selected_size;
+    },
     end:function(){
-      return this.first + this.count * this.step;
+      return this.selected_size + this.count * this.step;
     }
   },
   mounted() {
@@ -43,7 +54,7 @@ var JsonReader = new Vue({
   methods: {
     
     isEnd: function(){
-      return this.end > this.datalists.length;
+      return this.end > this.datalists;
     },
     
     seemore: function(){
